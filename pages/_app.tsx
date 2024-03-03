017f27dde2c2aca35dbd3dbc793f7567f569e0af
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-03-02 22:58:38 -0500

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
+
+function App({Component, pageProps}: AppProps) {
+  // 2. Wrap ChakraProvider at the root of your app
+  return (
+    <ChakraProvider>
+      <Component {...pageProps}/>
+    </ChakraProvider>
+  )
+}
+export default App
