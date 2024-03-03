eb0499ba7971de2d91a4e71e10f7017a10bf38c1
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-03-03 00:55:47 -0500

    Setup modal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,15 +5,18 @@ import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import {theme} from '../chakra/theme'
 import Layout from '../components/Layout/layout'
+import { RecoilRoot } from 'recoil'
 
 function App({Component, pageProps}: AppProps) {
   // 2. Wrap ChakraProvider at the root of your app
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </RecoilRoot>
   )
 }
 export default App
