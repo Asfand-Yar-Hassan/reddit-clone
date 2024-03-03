7959fbbbe117d22c89158a226e32d87477fde1bc
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-03-02 23:09:29 -0500

    Chakra ui styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,12 @@ import * as React from 'react'
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import {theme} from '../chakra/theme'
 
 function App({Component, pageProps}: AppProps) {
   // 2. Wrap ChakraProvider at the root of your app
   return (
-    <ChakraProvider>
+    <ChakraProvider theme = {theme}>
       <Component {...pageProps}/>
     </ChakraProvider>
   )
