b1ec1c374d09211892f854bf781ffd9e4fb7548e
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-03-03 19:00:49 -0500

    Error handling for signup

---
+++ src/firebase/errors.tsx
@@ -0,0 +1,6 @@
+export const FIREBASE_ERRORS = {
+  'Firebase: Error (auth/email-already-in-use)':
+    'A user already exists with this email',
+  'Firebase: Error (auth/invalid-password)': 'Invalid password or email',
+  'Firebase: Error (auth/user-not-found)': 'Invalid password or email',
+}
