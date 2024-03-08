5f412fa5ff70300b5e5a19634a9e23b60dc8f040
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-03-07 22:54:13 -0500

    Community page layout

---
+++ src/components/Community/NotFound.tsx
@@ -0,0 +1,17 @@
+import { Button, Flex, Link } from '@chakra-ui/react'
+
+const CommunityNotFound: React.FC = () => {
+  return (
+    <Flex
+      direction='column'
+      justify='center'
+      alignItems='center'
+      minHeight='60vh'>
+      Sorry, that community does not exist or has been banned
+      <Link href='/'>
+        <Button mt={4}>Go Home</Button>
+      </Link>
+    </Flex>
+  )
+}
+export default CommunityNotFound
---
+++ src/components/Layout/PageContent.tsx
@@ -0,0 +1,38 @@
+import { Flex } from '@chakra-ui/react'
+import React, { ReactPropTypes } from 'react'
+
+type PageContentProps = {
+  children: any
+}
+
+const PageContent: React.FC<PageContentProps> = ({ children }) => {
+  return (
+    // Parent container
+    <Flex border='1px solid red' justify='center' p='16px 0px'>
+      {/* Content container */}
+      <Flex
+        border='1px solid green'
+        width='95%'
+        justify='center'
+        maxWidth='860px'>
+        {/* Left Hand content container */}
+        <Flex
+          border='1px solid blue'
+          direction='column'
+          width={{ base: '100%', md: '65%' }}
+          mr={{ base: 0, md: 6 }}>
+          {children && children[0]}
+        </Flex>
+        {/* Right hand content container */}
+        <Flex
+          border='1px solid orange'
+          direction='column'
+          display={{ base: 'none', md: 'flex' }}
+          flexGrow={1}>
+          {children && children[1]}
+        </Flex>
+      </Flex>
+    </Flex>
+  )
+}
+export default PageContent
