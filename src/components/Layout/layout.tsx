aa78bdd2b8509e1ed7437f1b8531fe2d72b79c55
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-03-09 00:55:31 -0500

    Add vercel analytics

--- src/components/Layout/layout.tsx
+++ src/components/Layout/layout.tsx
@@ -1,17 +1,21 @@
-import React from "react";
-import Navbar from "../Navbar/Navbar"
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import { Analytics } from '@vercel/analytics/next'
 
 type LayoutProps = {
- children: any
+  children: any
 }
 
-const Layout:React.FC<LayoutProps> = ({children}) =>{
- return (
-  <>
-  <Navbar/>
-  <main>{children}</main>
-  </>
- )
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        {children}
+        <Analytics />
+      </main>
+    </>
+  )
 }
 
 export default Layout
