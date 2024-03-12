d9effd208cd3a1ef139425834525c962424f20e1
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-03-12 13:35:06 -0400

    refactor

--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -29,97 +29,99 @@ const UserMenu: React.FC<MenuProps> = ({ user }) => {
   const [authModalStateValue, setAuthModalState] =
     useRecoilState(authModalState)
   const resetCommunityState = useResetRecoilState(communityState)
-  
-  const logout = async()=>{
+
+  const logout = async () => {
     await signOut(auth)
     resetCommunityState()
   }
   return (
-    <Menu>
-      <MenuButton
-        cursor='pointer'
-        padding='0px 6px'
-        borderRadius={4}
-        _hover={{ outline: '1px solig', outlineColor: 'gray.200' }}>
-        <Flex align='center'>
+    <>
+      <Menu>
+        <MenuButton
+          cursor='pointer'
+          padding='0px 6px'
+          borderRadius={4}
+          _hover={{ outline: '1px solig', outlineColor: 'gray.200' }}>
           <Flex align='center'>
-            {user ? (
-              <>
-                <Icon
-                  as={FaRedditSquare}
-                  fontSize={24}
-                  mr={1}
-                  color='gray.300'
-                />
-                <Flex
-                  direction='column'
-                  display={{ based: 'none', lg: 'flex' }}
-                  fontSize='8pt'
-                  align='flex-start'
-                  mr={8}>
-                  <Text fontWeight={700}>
-                    {user?.displayName || user?.email?.split('@')[0]}
-                  </Text>
-                  <Flex>
-                    <Icon as={IoSparkles} color='brand.100' mr={1} />
-                    <Text color='gray.400'>1 karma</Text>
+            <Flex align='center'>
+              {user ? (
+                <>
+                  <Icon
+                    as={FaRedditSquare}
+                    fontSize={24}
+                    mr={1}
+                    color='gray.300'
+                  />
+                  <Flex
+                    direction='column'
+                    display={{ based: 'none', lg: 'flex' }}
+                    fontSize='8pt'
+                    align='flex-start'
+                    mr={8}>
+                    <Text fontWeight={700}>
+                      {user?.displayName || user?.email?.split('@')[0]}
+                    </Text>
+                    <Flex>
+                      <Icon as={IoSparkles} color='brand.100' mr={1} />
+                      <Text color='gray.400'>1 karma</Text>
+                    </Flex>
                   </Flex>
-                </Flex>
-              </>
-            ) : (
-              <Icon as={VscAccount} fontSize={24} color='gray.400' mr={1} />
-            )}
+                </>
+              ) : (
+                <Icon as={VscAccount} fontSize={24} color='gray.400' mr={1} />
+              )}
+            </Flex>
+            <ChevronDownIcon />
           </Flex>
-          <ChevronDownIcon />
-        </Flex>
-      </MenuButton>
-      <MenuList>
-        {user ? (
-          <>
-            <MenuItem
-              fontSize='10pt'
-              fontWeight={700}
-              _hover={{ bg: 'blue.500', color: 'white' }}>
-              <Flex align='center'>
-                <Icon as={CgProfile} fontSize={20} mr={2} />
-                Profile
-              </Flex>
-            </MenuItem>
-            <MenuDivider />
-            <MenuItem
-              fontSize='10pt'
-              fontWeight={700}
-              _hover={{ bg: 'blue.500', color: 'white' }}
-              onClick={ () => {
-                logout()
-              }}>
-              <Flex align='center'>
-                <Icon as={MdOutlineLogin} fontSize={20} mr={2} />
-                Log Out
-              </Flex>
-            </MenuItem>
-          </>
-        ) : (
-          <>
-            <MenuItem
-              fontSize='10pt'
-              fontWeight={700}
-              _hover={{ bg: 'blue.500', color: 'white' }}
-              onClick={() => {
-                setAuthModalState((prev) => ({
-                  open: true,
-                  view: 'login',
-                }))
-              }}>
-              <Flex align='center'>
-                <Icon as={MdOutlineLogin} fontSize={20} mr={2} />
-                Log In / Sign Up
-              </Flex>
-            </MenuItem>
-          </>
-        )}
-      </MenuList>
-    </Menu>
+        </MenuButton>
+        <MenuList>
+          {user ? (
+            <>
+              <MenuItem
+                fontSize='10pt'
+                fontWeight={700}
+                _hover={{ bg: 'blue.500', color: 'white' }}>
+                <Flex align='center'>
+                  <Icon as={CgProfile} fontSize={20} mr={2} />
+                  Profile
+                </Flex>
+              </MenuItem>
+              <MenuDivider />
+              <MenuItem
+                fontSize='10pt'
+                fontWeight={700}
+                _hover={{ bg: 'blue.500', color: 'white' }}
+                onClick={() => {
+                  logout()
+                }}>
+                <Flex align='center'>
+                  <Icon as={MdOutlineLogin} fontSize={20} mr={2} />
+                  Log Out
+                </Flex>
+              </MenuItem>
+            </>
+          ) : (
+            <>
+              <MenuItem
+                fontSize='10pt'
+                fontWeight={700}
+                _hover={{ bg: 'blue.500', color: 'white' }}
+                onClick={() => {
+                  setAuthModalState((prev) => ({
+                    open: true,
+                    view: 'login',
+                  }))
+                }}>
+                <Flex align='center'>
+                  <Icon as={MdOutlineLogin} fontSize={20} mr={2} />
+                  Log In / Sign Up
+                </Flex>
+              </MenuItem>
+            </>
+          )}
+        </MenuList>
+      </Menu>
+    </>
   )
 }
 export default UserMenu
