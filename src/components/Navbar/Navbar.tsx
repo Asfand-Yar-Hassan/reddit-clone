5b72b0f0b5600726e34b05ccd6e5fbd2b17fe81d
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-03-15 16:07:45 -0400

    Fix navbar image path

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -15,8 +15,11 @@ const Navbar: React.FC = () => {
       height='44px'
       padding='6px 12px'
       justify={{ md: 'space-between' }}>
-      <Flex align='center' width={{ base: '40px', md: 'auto' }} mr={{base:0, md:2}}>
-        <Image src='images/redditFace.svg' height='30px' />
+      <Flex
+        align='center'
+        width={{ base: '40px', md: 'auto' }}
+        mr={{ base: 0, md: 2 }}>
+        <Image src='/images/redditFace.svg' height='30px' />
         <Image
           src='/images/redditText.svg'
           height='46px'
