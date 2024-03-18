8579574f666a707dc2018c3f404cb1b5723cfccc
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-03-18 13:39:36 -0400

    Post link sharing functionality

--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -1,22 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  increment,
+  writeBatch,
+} from 'firebase/firestore'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
 import { useRecoilState, useSetRecoilState } from 'recoil'
+import { authModalState } from '../atoms/authModalAtom'
 import {
   Community,
   CommunitySnippet,
   communityState,
 } from '../atoms/communitiesAtom'
-import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, firestore } from '../firebase/clientApp'
-import {
-  collection,
-  doc,
-  getDocs,
-  increment,
-  writeBatch,
-} from 'firebase/firestore'
-import { authModalState } from '../atoms/authModalAtom'
 
 const useCommunityData = () => {
+  const router = useRouter()
   const [user] = useAuthState(auth)
   const setAuthModalState = useSetRecoilState(authModalState)
   const [loading, setLoading] = useState(false)
@@ -118,16 +121,41 @@ const useCommunityData = () => {
     setLoading(false)
   }
 
-  useEffect(()=>{
-    if(!user){
-      setCommunityStateValue((prev)=>({
+  const getCommunityData = async (communityId: string) => {
+    try {
+      const communityDocRef = doc(firestore, 'communities', communityId)
+      const communityDoc = await getDoc(communityDocRef)
+
+      setCommunityStateValue((prev) => ({
         ...prev,
-        mySnippets:[]
+        currentCommunity: {
+          id: communityDoc.id,
+          ...communityDoc.data(),
+        } as Community,
+      }))
+    } catch (error) {
+      console.log('getCommunityData error', error)
+    }
+  }
+
+  useEffect(() => {
+    if (!user) {
+      setCommunityStateValue((prev) => ({
+        ...prev,
+        mySnippets: [],
       }))
       return
     }
     getMySnippets()
   }, [user])
+
+  useEffect(() => {
+    const { communityId } = router.query
+
+    if (communityId && !communityStateValue.currentCommunity) {
+      getCommunityData(communityId as string)
+    }
+  }, [router.query, communityStateValue.currentCommunity])
 
   return {
     communityStateValue,
